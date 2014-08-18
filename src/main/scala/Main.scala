0b71d72c51943dc90e2b4780b909911c8b00041e
Patrick Reisert <kpreisert@googlemail.com>
2014-08-18 15:21:50 +0200

    Implemented some parsing of stylesheets (unfinished)

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -7,16 +7,18 @@ object Main {
   val stylesheet =
   <xsl:stylesheet version="1.0"
                   xmlns:xsl="http://www.w3.org/1999/XSL/Transform">
-    <html>
-      <head>
-        <title>Hello XHTML world</title>
-      </head>
-      <body>
-        <xsl:if test="true">
-          <h1>Hello world</h1>
-        </xsl:if>
-        <p><a href="scala-lang.org">Scala</a> talks XHTML</p>
-      </body>
-    </html>
+    <xsl:template match="/">
+      <html>
+        <head>
+          <title>Hello XHTML world</title>
+        </head>
+        <body>
+          <xsl:if test="true">
+            <h1>Hello world</h1>
+          </xsl:if>
+          <p><a href="scala-lang.org">Scala</a> talks XHTML</p>
+        </body>
+      </html>
+    </xsl:template>
   </xsl:stylesheet>;
 }
--- src/main/scala/XSLTStylesheet.scala
+++ src/main/scala/XSLTStylesheet.scala
@@ -1,18 +1,79 @@
 import scala.xml._
 
-class XSLTStylesheet(source: Elem) {
+class XSLTStylesheet(var source: Elem) {
   val XSLTNamespace = "http://www.w3.org/1999/XSL/Transform";
 
-  assert(source.namespace == XSLTNamespace, f"Root element must be 'stylesheet' with namespace $XSLTNamespace (a literal result element is not supported as root node)");
-  assert(source.attribute("version").get.text == "1.0", "Stylesheet version must be 1.0");
+  val trimmed = xml.Utility.trim(source).asInstanceOf[Elem]
+
+  assert(trimmed.namespace == XSLTNamespace, f"Root element must be 'stylesheet' with namespace $XSLTNamespace (a literal result element is not supported as root node)");
+  assert(trimmed.attribute("version").get.text == "1.0", "Stylesheet version must be 1.0");
+  assert(trimmed.child.forall(n => n.namespace == XSLTNamespace && (n.label == "template" || n.label == "variable")), "Top-level elements must either be XSLT templates or variable definitions");
+
+  // TODO: the spec requires us to evaluate variables in an order such that variables can depend on each other (as long
+  // as there are no circular dependencies, which would result in an error), see spec section 11.4
+  // var topLevelVariables = ...
+  var templates = trimmed.child
+    .filter(n => n.isInstanceOf[Elem] && n.label == "template")
+    .map(n => n.asInstanceOf[Elem])
+    .map(elem => TemplateElement(
+                  elem.attribute("name").map(_.text),
+                  elem.attribute("match").map(a => XPathExpression(a.text)),
+                  parseParams(elem.child),
+                  parseTemplate(elem.child.filter(n => !(n.isInstanceOf[Elem] && n.namespace == XSLTNamespace && n.label == "param")))
+                 ));
+
+  def parseTemplate(template: Seq[Node]) : Seq[XSLTNode] = {
+    // filter out comments first
+    template.filter(n => !n.isInstanceOf[Comment]).map(parseNode(_));
+  }
+
+  def parseNode(node: Node) : XSLTNode = {
+    if (node.isInstanceOf[Text]) {
+      LiteralTextNode(node.asInstanceOf[Text].data)
+    } else if (node.isInstanceOf[Elem]) {
+      if (node.namespace == XSLTNamespace) {
+        node.label match {
+          case "variable" => throw new UnsupportedOperationException("TODO") // TODO
+          case "apply-templates" => throw new UnsupportedOperationException("TODO") // TODO
+          case "call-template" => throw new UnsupportedOperationException("TODO") // TODO
+          case "attribute" => throw new UnsupportedOperationException("TODO") // TODO
+          case "value-of" => throw new UnsupportedOperationException("TODO") // TODO
+          case "choose" => throw new UnsupportedOperationException("TODO") // TODO
+          case "if" => throw new UnsupportedOperationException("TODO") // TODO
+        }
+      } else if (node.namespace == null) {
+        LiteralElement(node.label,
+          node.attributes.asAttrMap, // TODO: use SetAttributeElement for attributes instead (to simplify representation)?
+          parseTemplate(node.child)
+        )
+      } else {
+        throw new UnsupportedOperationException("Namespaces other than the XSLT namespace are not supported.")
+      }
+    } else {
+      throw new UnsupportedOperationException(f"Unsupported XML node $node")
+    }
+  }
+
+  def parseParams(input: Seq[Node]) : Map[String, Seq[XSLTNode]] = {
+    // TODO: support "select" attribute on params
+    val params = input.filter(n => n.isInstanceOf[Elem] && n.namespace == XSLTNamespace && n.label == "param")
+                      .map(n => n.asInstanceOf[Elem])
+                      .map(elem => (elem.attribute("name").get.text, parseTemplate(elem.child)))
+    Map() ++ params
+  }
 }
 
-abstract class XPathExpression
+case class TemplateElement(name: Option[String],
+                           matches: Option[XPathExpression],
+                           defaultParams: Map[String, Seq[XSLTNode]],
+                           content: Seq[XSLTNode])
+
+// TODO: parse XPath
+case class XPathExpression(expression: String)
 
 abstract class XSLTNode
-case class LiteralElement(name: String, attributes: Map[String, String] /*use SetAttributeElement for attributes instead?*/, children: Seq[XSLTNode]) extends XSLTNode
+case class LiteralElement(name: String, attributes: Map[String, String], children: Seq[XSLTNode]) extends XSLTNode
 case class LiteralTextNode(text: String) extends XSLTNode
-case class TemplateElement(name: Option[String], matches: Option[XPathExpression], defaultParams: Map[String, XSLTNode], content: Seq[XSLTNode]) extends XSLTNode
 case class VariableDefinitionElement(name: String, content: XSLTNode) extends XSLTNode
 case class ApplyTemplatesElement(select: XPathExpression, params: Map[String, XSLTNode]) extends XSLTNode
 case class CallTemplateElement(name: String, params: Map[String, XSLTNode]) extends XSLTNode
