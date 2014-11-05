7cf62259f4001013d93a4c2c96b35a8ce3e89c2b
Patrick Reisert <kpreisert@googlemail.com>
2014-11-05 15:45:50 +0100

    Some more (confusing) work on set-based zipper

--- src/main/scala/analysis/domain/zipper/set/ZipperTree.scala
+++ src/main/scala/analysis/domain/zipper/set/ZipperTree.scala
@@ -38,9 +38,45 @@ case class ZipperTreeNode(desc: NodeDescriptor, children: List[ZipperTree]) {
   }*/
 }
 
-//case class ZipperTree(s: Set[(NodeDescriptor, List[ZipperTree])]) TODO: (infinite) sets
-
 abstract class ZipperPath {
+  def getDescriptor: Option[Set[NodeDescriptor]]
+  def getParent: Option[Set[ZipperPath]]
+}
+
+case object InfZipperPath extends ZipperPath {
+  override def getDescriptor = None
+  override def getParent = None
+}
+
+case class FinParentZipperPath(parent: Set[ZipperPath]) extends ZipperPath {
+  override def getDescriptor = None
+  override def getParent = Some(parent)
+}
+
+case class FinZipperPath(set: Set[PathSegment]) extends ZipperPath {
+  override def getDescriptor: Option[Set[NodeDescriptor]] = Some(set.map(seg => seg.getDescriptor match {
+    case None => return None
+    case Some(desc) => desc
+  }))
+
+  override def getParent = Some(set.map {
+    case TopPathSegment => Nil
+    case NodePathSegment(_, _, parent, _) => List(parent)
+  }.flatten.toSet)
+}
+
+
+abstract class PathSegment {
+  def getDescriptor: Option[NodeDescriptor]
+}
+case object TopPathSegment extends PathSegment {
+  override def getDescriptor = Some(RootNode)
+}
+case class NodePathSegment(desc: Option[NodeDescriptor], left: List[ZipperTree], parent: ZipperPath, right: List[ZipperTree]) extends PathSegment {
+  override def getDescriptor = desc
+}
+
+/*abstract class ZipperPath {
   def getDescriptor: Option[Set[NodeDescriptor]]
 }
 case object TopPath extends ZipperPath {
@@ -54,7 +90,7 @@ case class NodePath(desc: Option[Set[NodeDescriptor]], left: List[ZipperTree], p
 
 case class InfNodePath(parent: ZipperPath) extends ZipperPath {
   def getDescriptor = None
-}
+}*/
 
 case class ZipperLoc(subtree: ZipperTree, path: ZipperPath) {
   /*def goLeft = this.path match {
@@ -75,19 +111,27 @@ case class ZipperLoc(subtree: ZipperTree, path: ZipperPath) {
   }*/
 
   def goDown = this.subtree match {
-    case InfZipperTree => ZipperLoc(InfZipperTree, InfNodePath(this.path))
+    case InfZipperTree => ZipperLoc(InfZipperTree, FinParentZipperPath(Set(this.path)))
     case FinZipperTree(s) =>
       val (subtrees, paths) = s.collect {
-        case ZipperTreeNode(desc, first :: children) => (first, NodePath(first.getDescriptor, Nil, this.path, children))
+        case ZipperTreeNode(desc, first :: children) => (first, makePath(first.getDescriptor, Nil, this.path, children))
       }.unzip
-      ZipperLoc(flattenZipperTreeSet(subtrees), ???)
-    // case ZipperTree(desc, first :: children) => ZipperLoc(first, NodePath(first.desc, Nil, this.path, children))
+      ZipperLoc(flattenZipperTreeSet(subtrees), flattenFinZipperPathSet(paths))
   }
 
-  private def flattenZipperTreeSet(set: Set[ZipperTree]): ZipperTree = {
+  private def flattenZipperTreeSet(set: Set[ZipperTree]): ZipperTree =
     FinZipperTree(set.map {
       case InfZipperTree => return InfZipperTree
       case FinZipperTree(s) => s
     }.flatten)
+
+  private def flattenFinZipperPathSet(set: Set[FinZipperPath]): FinZipperPath =
+    FinZipperPath(set.map(p => p.set).flatten)
+
+  private def makePath(desc: Option[Set[NodeDescriptor]], left: List[ZipperTree], parent: ZipperPath, right: List[ZipperTree]): FinZipperPath = {
+    desc match {
+      case None => FinZipperPath(Set(NodePathSegment(None, left, parent, right)))
+      case Some(s) => FinZipperPath(s.map(desc => NodePathSegment(Some(desc), left, parent, right)))
+    }
   }
 }
