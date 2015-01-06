ba02572ba7756222b2423d96f7c824b97fc7dfe7
Patrick Reisert <kpreisert@googlemail.com>
2015-01-06 17:21:29 +0100

    More notes and comments

--- src/main/scala/analysis/domain/zipper/Path.scala
+++ src/main/scala/analysis/domain/zipper/Path.scala
@@ -2,6 +2,7 @@ package analysis.domain.zipper
 
 import analysis.domain.Lattice
 
+/** Base class for descriptors (labels) of path steps. A descriptor describes a single node. */
 abstract class PathStepDescriptor
 
 case object AnyElementStep extends PathStepDescriptor {
@@ -23,12 +24,17 @@ case object AnyCommentNodeStep extends PathStepDescriptor {
   override def toString = "comment()"
 }
 
+/** Base class for paths. */
 abstract class Path
 
+/** The root path describes the root node. */
 case object RootPath extends Path {
   override def toString = "/"
 }
 
+/** The child step describes a node that is a child of another node.
+  * It contains a descriptor of the node itself and the parent path.
+  */
 case class ChildStep(descriptor: PathStepDescriptor, parent: Path) extends Path {
   override def toString = parent match {
     case RootPath => "/" + descriptor.toString
@@ -36,6 +42,9 @@ case class ChildStep(descriptor: PathStepDescriptor, parent: Path) extends Path 
   }
 }
 
+/** The descendant step describes a node that is a descendant of another node.
+  * It contains a descriptor of the node itself and the ancestor path, describing any ancestor.
+  */
 case class DescendantStep(descriptor: PathStepDescriptor, ancestor: Path) extends Path {
   override def toString = ancestor match {
     case RootPath => descriptor.toString
@@ -43,12 +52,17 @@ case class DescendantStep(descriptor: PathStepDescriptor, ancestor: Path) extend
   }
 }
 
-/** Helper object to parse patterns. */
+/** Helper object that contains
+  * - methods to parse patterns from XPath expressions
+  * - an implementation of the Lattice type class for sets of paths
+  */
 object Path {
   import xpath._
 
+  /** Creates a [[Path]] object from a given string, by first parsing the string using the [[XPathParser]]. */
   def fromString(str: String) = fromLocationPath(XPathParser.parse(str).asInstanceOf[LocationPath])
 
+  /** Creates a [[Path]] object given a [[LocationPath]]. */
   def fromLocationPath(path: LocationPath): Path = fromLocationPath(path.steps.reverse, path.isAbsolute)
 
   private def fromLocationPath(reverseSteps: List[XPathStep], isAbsolute: Boolean): Path = {
@@ -74,7 +88,13 @@ object Path {
     }
   }
 
+  /** This defines operations on the lattice of sets of abstract paths. */
   implicit object PathSetLattice extends Lattice[Set[Path]] {
+
+    /** This describes every possible path, because every node is either
+      * - the root node itself or
+      * - an element, attribute, comment or text node that is a descendant of the root node
+      */
     def top = Set(RootPath,
       DescendantStep(AnyElementStep, RootPath),
       DescendantStep(AnyAttributeStep, RootPath),
@@ -83,8 +103,12 @@ object Path {
 
     def bottom = Set()
 
+    /** Defines the JOIN operation for the lattice of path-sets. */
     def join(n1: Set[Path], n2: Set[Path]): Set[Path] = normalize(n1.union(n2))
 
+    /** Normalize the given set of paths in such a way that the resulting set contains
+      * no path that also describes (i.e. is greater than or equal to) another path in the set.
+      */
     protected def normalize(set: Iterable[Path]): Set[Path] = set.toList.foldRight(List[Path]()) {
       case (next, acc) => if (acc.exists(e => lessThanOrEqualSingle(next, e)))
         acc
@@ -92,6 +116,7 @@ object Path {
         next :: acc.filter(e => !lessThanOrEqualSingle(e, next))
     }.toSet
 
+    /** Defines the MEET operation (&&) for the lattice of path-sets. */
     def meet(n1: Set[Path], n2: Set[Path]): Set[Path] = {
       val result = n1.cross(n2).flatMap {
         case (p1, p2) => meetSingle(p1, p2)
@@ -99,6 +124,13 @@ object Path {
       normalize(result.toSet)
     }
 
+    /** Defines the MEET operation (&&) for single paths. The result is a set of paths with either
+      * - exactly one element, which is a path that was described by both input paths
+      *   (i.e. less than or equal to both operands)
+      * - no element (= BOTTOM), when no such common path exists
+      *
+      * Examples: /@foobar && /@* = Set(/@foobar), /a && /a/b = Set()
+      */
     protected def meetSingle(left: Path, right: Path): Set[Path] = (left, right) match {
       case (RootPath, RootPath) => Set(RootPath)
       case (ChildStep(desc1, prev1), ChildStep(desc2, prev2)) =>
@@ -130,6 +162,12 @@ object Path {
       case _ => Set() // mix of Root and Step -> BOTTOM
     }
 
+    /** Defines the MEET operation (&&) for descriptors. The result is
+      * - 'Some(desc)' with 'desc' being a descriptor that describes nodes that are described by both operands
+      * - 'None' if no such common descriptor exists (= BOTTOM)
+      *
+      * Examples: @foobar && @* = Some(@foobar), @foobar && * = None
+      */
     protected def meetDescriptor(desc1: PathStepDescriptor, desc2: PathStepDescriptor): Option[PathStepDescriptor] = {
       (desc1, desc2) match {
         case _ if desc1 == desc2 => Some(desc1)
@@ -141,9 +179,15 @@ object Path {
       }
     }
 
+    /** Defines the <= operation for the lattice of path-sets */
     def lessThanOrEqual(n1: Set[Path], n2: Set[Path]): Boolean =
       n1.forall(pat1 => n2.exists(pat2 => lessThanOrEqualSingle(pat1, pat2)))
 
+    /** Check if a single path is less than or equal to another path,
+      * i.e. if the left path is also described by the right path.
+      *
+      * Examples: /@foobar <= /@*, elem/@foobar <= elem//@foobar
+      */
     protected def lessThanOrEqualSingle(left: Path, right: Path): Boolean = (left, right) match {
       case (RootPath, RootPath) => true
       case (ChildStep(desc1, prev1), ChildStep(desc2, prev2)) =>
@@ -160,6 +204,10 @@ object Path {
       case _ => false // mix of Root and Step
     }
 
+    /** Check if a descriptor is less than or equal to another descriptor,
+      * i.e. if the right descriptor also describes all nodes that the left one describes.
+      * Example: @foobar <= @* (for attribute descriptors)
+      */
     protected def lessThanOrEqualDescriptor(desc1: PathStepDescriptor, desc2: PathStepDescriptor): Boolean = {
       (desc1, desc2) match {
         case _ if desc1 == desc2 => true // equal
@@ -169,7 +217,8 @@ object Path {
       }
     }
 
-    def getParent(path: Set[Path]) = normalize(path.collect {
+    /** Gets the path of the parent node of a given path. */
+    def getParent(path: Set[Path]): Set[Path] = normalize(path.collect {
       case ChildStep(desc, prev) => List(prev)
       case DescendantStep(desc, prev) => prev match {
         case RootPath => desc match {
@@ -181,6 +230,7 @@ object Path {
       }
     }.flatten)
 
+    /** Gets a list of the paths of all child nodes of a given path. */
     def getChildren(path: Set[Path]): ZList[Set[Path]] = ZList.joinAll(path.map {
       case e@ChildStep(AnyElementStep | NamedElementStep(_), _) => ZUnknownLength(Set[Path](ChildStep(AnyElementStep, e), ChildStep(AnyTextNodeStep, e), ChildStep(AnyCommentNodeStep, e)))
       case e@DescendantStep(AnyElementStep | NamedElementStep(_), _) => ZUnknownLength(Set[Path](ChildStep(AnyElementStep, e), ChildStep(AnyTextNodeStep, e), ChildStep(AnyCommentNodeStep, e)))
@@ -188,6 +238,7 @@ object Path {
       case _ => ZNil[Set[Path]]() // other node types (text, attribute) return empty lists because they don't have children
     })
 
+    /** Gets a list of the paths of all descendant nodes of a given path. */
     def getDescendants(path: Set[Path]): ZList[Set[Path]] = ZList.joinAll(path.map {
       case e@ChildStep(AnyElementStep | NamedElementStep(_), _) => ZUnknownLength(Set[Path](DescendantStep(AnyElementStep, e), DescendantStep(AnyTextNodeStep, e), DescendantStep(AnyCommentNodeStep, e)))
       case e@DescendantStep(AnyElementStep | NamedElementStep(_), _) => ZUnknownLength(Set[Path](DescendantStep(AnyElementStep, e), DescendantStep(AnyTextNodeStep, e), DescendantStep(AnyCommentNodeStep, e)))
@@ -195,13 +246,15 @@ object Path {
       case _ => ZNil[Set[Path]]() // other node types (text, attribute) return empty lists because they don't have children/descendants
     })
 
+    /** Gets a list of the paths of all attributes of a given node (described by its path). */
     def getAttributes(path: Set[Path]): ZList[Set[Path]] = ZList.joinAll(path.map {
       case e@ChildStep(AnyElementStep | NamedElementStep(_), _) => ZUnknownLength(Set[Path](ChildStep(AnyAttributeStep, e)))
       case e@DescendantStep(AnyElementStep | NamedElementStep(_), _) => ZUnknownLength(Set[Path](ChildStep(AnyAttributeStep, e)))
       case _ => ZNil[Set[Path]]() // other node types (text, attribute) return empty lists because they don't have attributes
     })
 
-    // TODO: depend on `normalize` for these predicates and remove them here?
+    // The following predicate functions could probably be removed, because the combination of these predicates
+    // for NodeDescriptor together with the normalization function make them unnecessary.
 
     def isElement(node: Set[Path]): (Set[Path], Set[Path]) = {
       val (yes, no) = node.partition {
