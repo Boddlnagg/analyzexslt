ae965a9cc3d5b084e8f7af977cb69d3c11a54788
Patrick Reisert <kpreisert@googlemail.com>
2015-02-05 18:20:05 +0100

    Refactored matches() function again and added section about it

--- src/main/scala/analysis/AbstractPatternMatcher.scala
+++ src/main/scala/analysis/AbstractPatternMatcher.scala
@@ -15,56 +15,26 @@ class AbstractPatternMatcher[N, L, V](xmlDom: XMLDomain[N, L, V]) {
     * Second result: nodes for which the path WILL definitely NOT match. When this is BOTTOM it
     *                means that the path will always match the given input.
     */
-  def matches(node: N, path: LocationPath): (N, N) = matches(node, path.steps.reverse, path.isAbsolute)
+  def matches(node: N, path: LocationPath): (N, N) =
+    matches(node, path.steps.reverse, path.isAbsolute)
 
-  def matches(node: N, reversedPathSteps: List[XPathStep], pathIsAbsolute: Boolean): (N, N) = {
+  private def matches(node: N, reversedPathSteps: List[XPathStep], pathIsAbsolute: Boolean): (N, N) = {
     // match recursively from right to left (path steps are reversed!)
     reversedPathSteps match {
       case Nil =>
-        // an empty path is always a match, but when it is an absolute path, the current node must be the root node
+        // an empty path is always a match, except when the path is absolute and the current node is not a root node
         if (pathIsAbsolute) xmlDom.isRoot(node) else (node, xmlDom.bottom)
-      case lastStep :: restPath =>
-        if (lastStep.predicates.nonEmpty) throw new NotImplementedError("predicates in paths are not implemented")
-        val (lastStepMatches, notLastStepMatches) = lastStep match {
-          // child::node()
-          case XPathStep(ChildAxis, AllNodeTest, Nil) =>
-            // NOTE: we assume here that there are no more node types that element, text, comment, root and attribute
-            val matches = xmlDom.joinAll(List(xmlDom.isElement(node)._1, xmlDom.isTextNode(node)._1, xmlDom.isComment(node)._1))
-            val notMatches = xmlDom.join(xmlDom.isRoot(node)._1, xmlDom.isAttribute(node)._1)
-            (matches, notMatches)
-          // child::comment()
-          case XPathStep(ChildAxis, CommentNodeTest, Nil) => xmlDom.isComment(node)
-          // child::text()
-          case XPathStep(ChildAxis, TextNodeTest, Nil) => xmlDom.isTextNode(node)
-          // child::*
-          case XPathStep(ChildAxis, NameTest(None, "*"), Nil) => xmlDom.isElement(node)
-          // child::name
-          case XPathStep(ChildAxis, NameTest(None, name), Nil) =>
-            val (element, notElement) = xmlDom.isElement(node)
-            val (hasName, notHasName) = xmlDom.hasName(element, name)
-            (hasName, xmlDom.join(notElement, notHasName))
-          // attribute::* OR attribute::node()
-          case XPathStep(AttributeAxis, NameTest(None, "*") | AllNodeTest, Nil) => xmlDom.isAttribute(node)
-          // attribute::name
-          case XPathStep(AttributeAxis, NameTest(None, name), Nil) =>
-            val (attr, notAttr) = xmlDom.isAttribute(node)
-            val (hasName, notHasName) = xmlDom.hasName(attr, name)
-            (hasName, xmlDom.join(notAttr, notHasName))
-          // attribute::comment() OR attribute::text() [these can never match anything]
-          case XPathStep(AttributeAxis, CommentNodeTest | TextNodeTest, _) => (xmlDom.bottom, node)
-          // any step using a name test with a prefixed name
-          case XPathStep(_, NameTest(Some(_), _), Nil) => throw new NotImplementedError("Prefixed names are not implemented")
-        }
-
-        if (!xmlDom.lessThanOrEqual(lastStepMatches, xmlDom.bottom)) restPath match {
+      case nextStep :: restPath =>
+        val (nextStepMatches, notNextStepMatches) = matchesSingleStep(node, nextStep)
+        if (!xmlDom.lessThanOrEqual(nextStepMatches, xmlDom.bottom)) restPath match {
           case XPathStep(DescendantOrSelfAxis, AllNodeTest, Nil) :: Nil =>
             // special case for the trivial '//' step as leftmost step in the path, which always matches
             // because every node is a descendant of the root node
             assert(pathIsAbsolute)
-            (lastStepMatches, notLastStepMatches)
+            (nextStepMatches, notNextStepMatches)
           case XPathStep(DescendantOrSelfAxis, AllNodeTest, Nil) :: restTail =>
             // the next step is '//' and must be handled separately (does any ancestor match the rest of the path?)
-            var current = lastStepMatches
+            var current = nextStepMatches
             var ancestorMatches = xmlDom.bottom
             var notAncestorMatches = node
             var (root, notRoot) = xmlDom.isRoot(current)
@@ -94,14 +64,14 @@ class AbstractPatternMatcher[N, L, V](xmlDom: XMLDomain[N, L, V]) {
               notRoot = newNotRoot
               nodeStack += notRoot
             }
-            (ancestorMatches, xmlDom.join(notAncestorMatches, notLastStepMatches))
+            (ancestorMatches, xmlDom.join(notAncestorMatches, notNextStepMatches))
           case _ =>
             // does the parent match the rest of the path?
-            val parent = xmlDom.getParent(lastStepMatches)
+            val parent = xmlDom.getParent(nextStepMatches)
             val (parentMatchesRest, _) = matches(parent, restPath, pathIsAbsolute)
-            val (parentMatches, notParentMatches) = hasParent(lastStepMatches, parentMatchesRest)
-            (parentMatches, xmlDom.join(notParentMatches, notLastStepMatches))
-        } else (xmlDom.bottom, notLastStepMatches)
+            val (parentMatches, notParentMatches) = hasParent(nextStepMatches, parentMatchesRest)
+            (parentMatches, xmlDom.join(notParentMatches, notNextStepMatches))
+        } else (xmlDom.bottom, notNextStepMatches)
     }
   }
 
@@ -114,6 +84,38 @@ class AbstractPatternMatcher[N, L, V](xmlDom: XMLDomain[N, L, V]) {
     val (isChild, isNotChild) = xmlDom.isContainedIn(node, xmlDom.getChildren(parent))
     val (isAttribute, isNeither) = xmlDom.isContainedIn(isNotChild, xmlDom.getAttributes(parent))
     (xmlDom.join(isChild, isAttribute), isNeither)
+  }
+
+  private def matchesSingleStep(node: N, step: XPathStep): (N, N) = step match {
+    // child::node()
+    case XPathStep(ChildAxis, AllNodeTest, Nil) =>
+      // NOTE: we assume here that there are no more node types that element, text, comment, root and attribute
+      val matches = xmlDom.joinAll(List(xmlDom.isElement(node)._1, xmlDom.isTextNode(node)._1, xmlDom.isComment(node)._1))
+      val notMatches = xmlDom.join(xmlDom.isRoot(node)._1, xmlDom.isAttribute(node)._1)
+      (matches, notMatches)
+    // child::comment()
+    case XPathStep(ChildAxis, CommentNodeTest, Nil) => xmlDom.isComment(node)
+    // child::text()
+    case XPathStep(ChildAxis, TextNodeTest, Nil) => xmlDom.isTextNode(node)
+    // child::*
+    case XPathStep(ChildAxis, NameTest(None, "*"), Nil) => xmlDom.isElement(node)
+    // child::name
+    case XPathStep(ChildAxis, NameTest(None, name), Nil) =>
+      val (element, notElement) = xmlDom.isElement(node)
+      val (hasName, notHasName) = xmlDom.hasName(element, name)
+      (hasName, xmlDom.join(notElement, notHasName))
+    // attribute::* OR attribute::node()
+    case XPathStep(AttributeAxis, NameTest(None, "*") | AllNodeTest, Nil) => xmlDom.isAttribute(node)
+    // attribute::name
+    case XPathStep(AttributeAxis, NameTest(None, name), Nil) =>
+      val (attr, notAttr) = xmlDom.isAttribute(node)
+      val (hasName, notHasName) = xmlDom.hasName(attr, name)
+      (hasName, xmlDom.join(notAttr, notHasName))
+    // attribute::comment() OR attribute::text() [these can never match anything]
+    case XPathStep(AttributeAxis, CommentNodeTest | TextNodeTest, _) => (xmlDom.bottom, node)
+    // any step using a name test with a prefixed name or a non-empty list of predicates
+    case XPathStep(_, NameTest(Some(_), _), Nil) => throw new NotImplementedError("Prefixed names are not implemented")
+    case XPathStep(_, _, _ :: _) => throw new NotImplementedError("Predicates in paths are not implemented")
   }
 }
 
--- src/main/scala/xslt/XSLTPatternMatcher.scala
+++ src/main/scala/xslt/XSLTPatternMatcher.scala
@@ -7,37 +7,14 @@ object XSLTPatternMatcher {
   /** Returns a value indicating whether a given node matches a location path pattern.
     * NOTE: only supports location path patterns (XSLT spec section 5.2) without predicates
     */
-  def matches(node: XMLNode, path: LocationPath): Boolean = matches(node, path.steps.reverse, path.isAbsolute)
+  def matches(node: XMLNode, path: LocationPath): Boolean =
+    matches(node, path.steps.reverse, path.isAbsolute)
 
-  def matches(node: XMLNode, reversedPathSteps: List[XPathStep], pathIsAbsolute: Boolean): Boolean = {
+  private def matches(node: XMLNode, reversedPathSteps: List[XPathStep], pathIsAbsolute: Boolean): Boolean = {
     // match recursively from right to left (path steps are reversed!)
     reversedPathSteps match {
-      case Nil =>
-        // an empty relative path is always a match, but when it is an absolute path, the current node must be the root node
-        if (pathIsAbsolute) node.isInstanceOf[XMLRoot] else true
-      case lastStep :: restPath =>
-        if (lastStep.predicates.nonEmpty) throw new NotImplementedError("predicates in paths are not implemented")
-        val lastStepMatches = lastStep match {
-          // child::node()
-          case XPathStep(ChildAxis, AllNodeTest, Nil) => node.isInstanceOf[XMLElement] | node.isInstanceOf[XMLTextNode] | node.isInstanceOf[XMLComment]
-          // child::comment()
-          case XPathStep(ChildAxis, CommentNodeTest, Nil) => node.isInstanceOf[XMLComment]
-          // child::text()
-          case XPathStep(ChildAxis, TextNodeTest, Nil) => node.isInstanceOf[XMLTextNode]
-          // child::*
-          case XPathStep(ChildAxis, NameTest(None, "*"), Nil) => node.isInstanceOf[XMLElement]
-          // child::name
-          case XPathStep(ChildAxis, NameTest(None, name), Nil) => node.isInstanceOf[XMLElement] && node.asInstanceOf[XMLElement].name == name
-          // attribute::*
-          case XPathStep(AttributeAxis, NameTest(None, "*") | AllNodeTest, Nil) => node.isInstanceOf[XMLAttribute]
-          // attribute::name
-          case XPathStep(AttributeAxis, NameTest(None, name), Nil) => node.isInstanceOf[XMLAttribute] && node.asInstanceOf[XMLAttribute].name == name
-          // attribute::comment() OR attribute::text() [these can never match anything]
-          case XPathStep(AttributeAxis, CommentNodeTest | TextNodeTest, _) => false
-          // any step using a name test with a prefixed name
-          case XPathStep(_, NameTest(Some(_), _), Nil) => throw new NotImplementedError("Prefixed names are not implemented")
-        }
-        if (lastStepMatches) {
+      case nextStep :: restPath =>
+        if (matchesSingleStep(node, nextStep)) {
           restPath match {
             case XPathStep(DescendantOrSelfAxis, AllNodeTest, Nil) :: Nil =>
               // special case for the trivial '//' step as leftmost step in the path, which always matches
@@ -45,11 +22,39 @@ object XSLTPatternMatcher {
               assert(pathIsAbsolute)
               true
             case XPathStep(DescendantOrSelfAxis, AllNodeTest, Nil) :: restTail =>
-              // the next step is '//' and must be handled separately (does any ancestor match the rest of the path?)
+              // the next step is '//' and must be handled separately:
+              // does any ancestor match the rest of the path?
               node.ancestors.exists(a => matches(a, restTail, pathIsAbsolute))
-            case _ => matches(node.parent, restPath, pathIsAbsolute) // does the parent match the rest of the path?
+            case _ =>
+              // does the parent match the rest of the path?
+              matches(node.parent, restPath, pathIsAbsolute)
           }
         } else { false }
+      case Nil =>
+        // an empty path is always a match, except when the path is absolute and the current node is not a root node
+        if (pathIsAbsolute) node.isInstanceOf[XMLRoot] else true
     }
+  }
+
+  private def matchesSingleStep(node: XMLNode, step: XPathStep) = step match {
+    // child::node()
+    case XPathStep(ChildAxis, AllNodeTest, Nil) => node.isInstanceOf[XMLElement] | node.isInstanceOf[XMLTextNode] | node.isInstanceOf[XMLComment]
+    // child::comment()
+    case XPathStep(ChildAxis, CommentNodeTest, Nil) => node.isInstanceOf[XMLComment]
+    // child::text()
+    case XPathStep(ChildAxis, TextNodeTest, Nil) => node.isInstanceOf[XMLTextNode]
+    // child::*
+    case XPathStep(ChildAxis, NameTest(None, "*"), Nil) => node.isInstanceOf[XMLElement]
+    // child::name
+    case XPathStep(ChildAxis, NameTest(None, name), Nil) => node.isInstanceOf[XMLElement] && node.asInstanceOf[XMLElement].name == name
+    // attribute::*
+    case XPathStep(AttributeAxis, NameTest(None, "*") | AllNodeTest, Nil) => node.isInstanceOf[XMLAttribute]
+    // attribute::name
+    case XPathStep(AttributeAxis, NameTest(None, name), Nil) => node.isInstanceOf[XMLAttribute] && node.asInstanceOf[XMLAttribute].name == name
+    // attribute::comment() OR attribute::text() [these can never match anything]
+    case XPathStep(AttributeAxis, CommentNodeTest | TextNodeTest, _) => false
+    // any step using a name test with a prefixed name or a non-empty list of predicates
+    case XPathStep(_, NameTest(Some(_), _), Nil) => throw new NotImplementedError("Prefixed names are not implemented")
+    case XPathStep(_, _, _ :: _) => throw new NotImplementedError("Predicates in paths are not implemented")
   }
 }
